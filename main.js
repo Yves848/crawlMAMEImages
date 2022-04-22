6f477d662d9064ba4b8a83268340851dc04ed2ca
yves848 <yves.godart@gmail.com>
2022-04-22 09:38:45 +0200

    Download ok

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer';
+import fetch from 'node-fetch';
+import fs from 'fs';
 
-(async () => {
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
+async function getLinks(sUrl) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://thumbnails.libretro.com/MAME/Named_Boxarts/');
+    await page.goto(sUrl);
     let urls = await page.evaluate(() => {
         let results = [];
         let items = document.querySelectorAll('a');
         console.log(items);
+
         items.forEach((item) => {
+            const url = item.getAttribute('href');
+            const text = decodeURI(url);
             results.push({
-                url: item.getAttribute('href'),
-                text: item.innerHTML
+                url,
+                text
             });
         });
+        return results;
     });
     browser.close();
-    return results;
+    return urls;
+}
+
+async function writeImage(res, sFile) {
+    if (!fs.statSync('./Downloads').isDirectory())
+        fs.mkdirSync('./Downloads');
+    if (sFile !== '../') {
+        console.log('Téléchargement ' + sFile);
+        const stream = fs.createWriteStream('./Downloads/' + sFile);
+        stream.on("finish", () => {
+            return;
+        });
+        res.body.pipe(stream);
+    }
+
+}
+
+(async () => {
+    const url = 'http://thumbnails.libretro.com/MAME/Named_Boxarts/';
+    const links = await getLinks(url);
+    asyncForEach(links, async (link, i) => {
+        //console.table({ "url": link.url, "text": link.text });
+        const res = await fetch(url + link.url)
+        await writeImage(res, link.text);
+    });
 })();
